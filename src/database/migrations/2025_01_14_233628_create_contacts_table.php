856063415df9ac87a21c94cb5cbf50b22ebe67dd
sumire sato <sumire.8007@gmail.com>
2025-01-15 00:47:24 +0900

    コミット1回目

---
+++ src/database/migrations/2025_01_14_233628_create_contacts_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateContactsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('contacts', function (Blueprint $table) {
+            $table->id();
+            $table->foreignID('category_id')->constrained()->cascadeOnDelete();
+            $table->string('first_name',20);
+            $table->string('last_name',20);
+            $table->enum('gender', ['male', 'female', 'other']);
+            $table->string('email');
+            $table->string('tell', 11);
+            $table->string('address',255)
+            $table->string('building',255);
+            $table->text('detail');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('contacts');
+    }
+}
---
+++ src/resources/views/index.blade.php
@@ -0,0 +1,12 @@
+<!-- <お問い合わせフォーム入力ページ -->
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>FashionablyLate</title>
+</head>
+<body>
+    <h1>Hello</h1>
+</body>
+</html>
